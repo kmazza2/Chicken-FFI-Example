f2eb40b2db3bc6981b6b2e48cbfac1c0ce91a052
Kevin <kspam@sent.com>
2025-02-09 16:33:48 +0000

    Return new vector

--- hello.scm
+++ hello.scm
@@ -6,5 +6,12 @@
     )
   )
   (define IncVec (foreign-lambda integer "IncVec" f64vector integer))
-  (define (ScmIncVec input) (IncVec input (f64vector-length input)))
+  (define
+    (ScmIncVec input)
+    (let
+      ((output (blob->f64vector (f64vector->blob input))))
+      (IncVec output (f64vector-length output))
+      output
+    )
+  )
 )
